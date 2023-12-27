dc5310496b265aa0ee7707e2b51f439adcaaa8a5
krestovsky13 <nikkres@mail.ru>
2023-12-27 13:07:40 +0300

    refactoring

--- bet-maker/src/amqp.py
+++ bet-maker/src/amqp.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import aio_pika
 from aio_pika.abc import AbstractRobustConnection
-from asyncpg import Pool
+from asyncpg import Pool, exceptions
 from dotenv import load_dotenv
 from fastapi.logger import logger
 
@@ -20,28 +20,50 @@ RMQ_PASSWORD = os.getenv('RABBITMQ_DEFAULT_PASS', 'guest')
 
 
 async def update_bets(event_id: str, state: int) -> None:
-    pool: Pool = await create_database_pool()
-    async with pool.acquire() as connection:
-        query = "UPDATE bets SET state = $1 WHERE event_id = $2;"
-        res = await connection.execute(query, state, event_id)
-        logger.info(f'Update bets completed: {res.split()[-1]} records')
+    while True:
+        try:
+            pool: Pool = await create_database_pool()
+            async with pool.acquire() as connection:
+                query = "UPDATE bets SET state = $1 WHERE event_id = $2;"
+                res = await connection.execute(query, state, event_id)
+                logger.info(f'Update bets completed: {res.split()[-1]} records')
+        except exceptions.PostgresConnectionError as e:
+            logger.error(f'PostgreSQL connection error: {str(e)}')
+            await asyncio.sleep(5)
+        else:
+            break
 
 
 async def message_consumer() -> None:
-    connection: AbstractRobustConnection = await aio_pika.connect_robust(
-        f'amqp://{RMQ_USER}:{RMQ_PASSWORD}@rabbitmq/',
-        loop=asyncio.get_event_loop(),
-    )
-    logger.info('Connection with broker has been successfully established')
-    async with connection:
-        channel = await connection.channel()
+    while True:
+        try:
+            connection = await connect_to_rabbitmq()
+            logger.info('Connection with broker has been successfully established')
+            async with connection:
+                channel = await connection.channel()
 
-        queue = await channel.declare_queue('state_queue')
-        logger.info('Waiting incoming messages')
+                queue = await channel.declare_queue('state_queue')
+                logger.info('Waiting incoming messages')
 
-        async with queue.iterator() as queue_iter:
-            async for message in queue_iter:
-                event_id, state = json.loads(message.body.decode('utf-8')).values()
-                logger.info(f'Incoming message from "{message.app_id}" (size: {message.body_size} bytes)')
-                await update_bets(event_id, state)
-                await message.ack()
+                async with queue.iterator() as queue_iter:
+                    async for message in queue_iter:
+                        event_id, state = json.loads(message.body.decode('utf-8')).values()
+                        logger.info(f'Incoming message from "{message.app_id}" (size: {message.body_size} bytes)')
+                        await update_bets(event_id, state)
+                        await message.ack()
+        except Exception as e:
+            logger.error(f'An error occurred: {str(e)}')
+            await asyncio.sleep(5)
+
+
+async def connect_to_rabbitmq() -> AbstractRobustConnection:
+    while True:
+        try:
+            connection = await aio_pika.connect_robust(
+                f'amqp://{RMQ_USER}:{RMQ_PASSWORD}@rabbitmq/',
+                loop=asyncio.get_event_loop(),
+            )
+            return connection
+        except Exception as e:
+            logger.error(f'Failed to connect to RabbitMQ: {str(e)}')
+            await asyncio.sleep(5)
--- bet-maker/src/api.py
+++ bet-maker/src/api.py
@@ -1,5 +1,9 @@
+import time
+from typing import Optional
+
 import aiohttp
 from asyncpg import Pool
+from cachetools import TTLCache, cached
 from fastapi import APIRouter, Depends, status, HTTPException
 
 from src.db import get_db_pool
@@ -8,6 +12,7 @@ from src.models import Event, BaseBet, Bet, CreateBet
 router = APIRouter()
 
 
+@cached(TTLCache(maxsize=128, ttl=30))
 async def get_open_events():
     async with aiohttp.ClientSession() as session:
         url = 'http://line-provider:8000/api/events'
@@ -16,15 +21,25 @@ async def get_open_events():
             return data
 
 
+async def get_event(event_id: str) -> Optional[dict]:
+    async with aiohttp.ClientSession() as session:
+        url = f'http://line-provider:8000/api/event/{event_id}'
+        async with session.get(url) as response:
+            if response.status == status.HTTP_200_OK:
+                return await response.json()
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Event not found')
+
+
 @router.post('/bet', response_model=BaseBet, status_code=status.HTTP_201_CREATED)
 async def create_bet(bet: CreateBet, pool: Pool = Depends(get_db_pool)):
-    if any(event.get('event_id') == bet.event_id for event in await get_open_events()):
-        async with pool.acquire() as connection:
-            query = "INSERT INTO bets (event_id, amount) VALUES ($1, $2) RETURNING bet_id"
-            bet_id = await connection.fetchval(query, bet.event_id, bet.amount)
-
-        return Bet(bet_id=str(bet_id))
-    raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail='Event not available')
+    event = await get_event(bet.event_id)
+    if deadline := event.get('deadline', None):
+        if deadline > time.time():
+            async with pool.acquire() as connection:
+                query = "INSERT INTO bets (event_id, amount) VALUES ($1, $2) RETURNING bet_id"
+                bet_id = await connection.fetchval(query, bet.event_id, bet.amount)
+            return Bet(bet_id=str(bet_id))
+    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Event not available')
 
 
 @router.get('/events', response_model=list[Event], status_code=status.HTTP_200_OK)
